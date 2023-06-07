ea56f90b4ca4b76e6767b207046bdc127162b6d2
Brendan Moy <bmoy@JN165G9JN1.grubhub.local>
2023-06-07 16:43:03 -0400

    fix build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 bundle config set without development test
 bundle config set force_ruby_platform true
+bundle lock --add-platform x86-mingw32 x86-mswin32 x64-mingw32 java
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
