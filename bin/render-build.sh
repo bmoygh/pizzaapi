bbe885690859b5ae92824bc1be78902034a39126
Brendan Moy <bmoy@JN165G9JN1.grubhub.local>
2023-06-07 11:43:19 -0400

    edit render yaml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+bundle config set without development test
+bundle config set force_ruby_platform true
 bundle install
+bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
